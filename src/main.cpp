6016ba36d73c693d6c783753dbeadebd64a7e97e
zyphorah <zyphorah.zen@gmail.com>
2024-06-14 12:23:43 -0400

    V6

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 #include "../include/Matrice3D.h"
 #include "../include/keycontrole.h"
 
-void collision(vector<vector>Vector3f>> tableauMatrixCube)
+void collision(vector<vector<Vector3f>>tableauMatrixCube,RenderWindow&window,int tailleSnake)
 {
-    
+    for(int i = 1; i<tailleSnake;i++)
+    {
+        //Regarder Si là tête n'est pas en contacte avec le corp
+        if(tableauMatrixCube[0] == tableauMatrixCube[i])
+        {
+            window.close();
+        }
+    }
 }
 
 void dessinerCube(vector<vector<Vector3f>> tableauMatrixCube,RenderWindow& window,int tailleSnake, Vector3f pivotPointAxe,Vector2f& angleRotation,Vector3f&DirectionPivot)
@@ -17,13 +24,13 @@ void dessinerCube(vector<vector<Vector3f>> tableauMatrixCube,RenderWindow& windo
          dessinerVecteur(projectionCube,window); 
     }    
 }
-void deplacementSnake(vector<vector<Vector3f>>& tableauMatrixCube, int& tailleSnake, Vector3f& direction) 
+void deplacementSnake(vector<vector<Vector3f>>& tableauMatrixCube, int& tailleSnake, Vector3f& direction,int& grandirSnake) 
 {
     //Stocker la tête car ont n'a besoins des coordonnées pour la génération de la nouvelle tête
     Vector3f initCoord{tableauMatrixCube[0][0]};
 
     // Déplacer chaque segment à la position du segment précédent
-    for (int i = tailleSnake - 1; i > 0; --i) 
+    for (int i = tableauMatrixCube.size()-1; i > 0; --i) 
     {
         tableauMatrixCube[i] = tableauMatrixCube[i - 1];
     }
@@ -31,6 +38,13 @@ void deplacementSnake(vector<vector<Vector3f>>& tableauMatrixCube, int& tailleSn
     // Créer un nouveau cube pour la tête du snake dans la nouvelle direction
     initCoord+= direction;
     genererCube(tableauMatrixCube[0],initCoord,20);
+    grandirSnake++;
+    if(grandirSnake == 4)
+    {
+        tailleSnake++;
+        grandirSnake=0;
+    }
+
 }
 
 bool timeValid(Time elapsed,int conditionTime)
@@ -48,8 +62,10 @@ void loopWindow(RenderWindow& window,Event& event)
     Vector3f initCoord{500, 300, 0};
 
     //Stockage du serpent dans un tableau de matrice
-    vector<vector<Vector3f>> tableauDeMatrixCube(500);
-    int tailleSnake{10};
+    vector<vector<Vector3f>> tableauDeMatrixCube(1000);
+    int tailleSnake{3};
+    //Utiliser pour gérer le temps, incrémentation chaque seconde
+    int grandirSnake{0};
     Vector3f direction{-20,0,0};
 
     //Gestion du temps pour le déplacement snake
@@ -69,11 +85,12 @@ void loopWindow(RenderWindow& window,Event& event)
         elapsed = clock.getElapsedTime();
         if(timeValid(elapsed,conditionTime))
         {
-            deplacementSnake(tableauDeMatrixCube,tailleSnake,direction);
+            deplacementSnake(tableauDeMatrixCube,tailleSnake,direction,grandirSnake);
             elapsed = clock.restart(); 
         }
        
         eventKey(window,pivotPointAxe,DirectionPivot,direction);
+        collision(tableauDeMatrixCube,window,tailleSnake);
         dessinerCube(tableauDeMatrixCube,window,tailleSnake,pivotPointAxe,angleRotation,DirectionPivot);
         
         window.display();
